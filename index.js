f4cd78c7c56dd70a6a5df3ab1dff0f21c663f37c
kevin <kevin.e@qq.com>
2019-07-15 16:19:50 +0800

    电话号码整理

--- index.js
+++ index.js
@@ -12,24 +12,28 @@ fs.readdir('./input', function(err,files){
             // 删除列名称字段
             sheet.data.splice(0,1);
             tempSheet = [];
-            // 暂存多个邮箱的行
+            // 暂存多个电话的行
             for (let i = 0; i < sheet.data.length; i++) {
                 if(sheet.data[i][0] == undefined){
                     // 行尾
                     break;
                 }
-                if(sheet.data[i][14] == undefined){
-                    // 跳过 没有邮箱的列
+                if(sheet.data[i][10] == undefined){
+                    // 跳过 没有电话的列
                     continue;
                 }
-                var emails = sheet.data[i][14].trim();
-                if(emails[emails.indexOf(";",emails.length - 1)] == ";"){
-                    emails = emails.slice(0,emails.length - 2); 
+                if(sheet.data[i][11] != undefined){
+                    sheet.data[i][10] = sheet.data[i][10] + ";" + sheet.data[i][11]; 
                 }
-                emails = emails.split(";");
-                for (let n = 0; n < emails.length; n++) {
-                    if(emails[n] != '' && emails[n] != '...'){
-                        tempSheet.push([emails[n].trim(),sheet.data[i][0]]);
+                var phones = sheet.data[i][10].trim();
+                if(phones[phones.indexOf(";",phones.length - 1)] == ";"){
+                    phones = phones.slice(0,phones.length - 1); 
+                }
+                phones = phones.split(";");
+                var contact = sheet.data[i][0].slice(0,sheet.data[i][0].indexOf("有限公司")) +"-"+ sheet.data[i][1];
+                for (let n = 0; n < phones.length; n++) {
+                    if(phones[n] != '' && phones[n].length == 11 && phones[n].indexOf("-") == -1 && phones[n] != '...'){
+                        tempSheet.push([contact,phones[n].trim()]);
                     };
                 }
             }
